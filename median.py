091dab20961e36235e8a0755d575602a45f4a02a
mantraak <mantraa.k07@gmail.com>
2026-06-19 10:00:41 +0530

    Add median of two sorted arrays solution

---
+++ median.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        num = sorted(nums1 + nums2)
+        n = len(num)
+        if n % 2 == 0:
+            return (num[n//2 - 1] + num[n//2]) / 2
+        else:
+            return num[n//2]
